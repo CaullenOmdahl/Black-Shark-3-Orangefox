e7ea226c4df0633ec1c8aefd76d2250e04b56c1f
CaullenOmdahl <77470883+CaullenOmdahl@users.noreply.github.com>
2024-11-13 00:50:43 +0700

    Update build_orangefox.sh

--- build_orangefox.sh
+++ build_orangefox.sh
@@ -42,7 +42,7 @@ install_packages() {
     print_status "Installing required packages..."
     sudo apt update
     sudo apt install -y \
-        git-core gnupg flex bison build-essential zip curl zlib1g-dev \
+        git-core gnupg repo flex bison build-essential zip curl zlib1g-dev \
         gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses-dev \
         x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils \
         xsltproc unzip fontconfig aria2 \
